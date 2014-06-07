d31461f05af01de7b5be9cf1a6371d967c4c3892
Abhay Mittal <abhaymittal@y7mail.com>
2014-06-07 23:39:58 +0530

    Minor changes to var.h and added var.cpp

---
+++ var.cpp
@@ -0,0 +1,42 @@
+    var::get_var()
+    {
+        return var_name;
+    }
+
+    var::set_var(char var_x
+    {
+        var_name=var_x;
+        return 0;
+    }
+
+    var::get_index()
+    {
+        return index;
+    }
+
+    var::set_index(int in_val)
+    {
+        index=in_val;
+        return 0;
+    }
+
+    var::get_delta()
+    {
+        return delta;
+    }
+
+    var::set_delta(int del)
+    {
+        delta=del;
+        return 0;
+    }
+
+    var::get_value()
+    {
+        return value;
+    }
+    var::set_value(int val)
+    {
+        value=val;
+        return 0;
+    }
--- var.h
+++ var.h
@@ -6,23 +6,23 @@ class var
 {
     private:
 
-    char var;
+    char var_name;
     int index;
     int delta;
     int value;
 
     public:
 
-    char get_var();
-    int set_var(char var_name);
+    char get_var_name();
+    int set_var_name(char var_x);
 
     int get_index();
     int set_index(int in_val);
 
     int get_delta();
-    int set_delta();
+    int set_delta(int del);
 
     int get_value();
-    int set_value();
+    int set_value(int val);
 }
 #endif // VAR_H
