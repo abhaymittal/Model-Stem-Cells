8d8b07c5b1bd5922b2ef2d89732f4e0a15e1e4fb
Pratik Varshney <pratz.varz@gmail.com>
2014-06-07 17:39:41 +0530

    Added stem_env class header

---
+++ stem_env.h
@@ -0,0 +1,7 @@
+
+class stem_env
+{
+public:
+    draw_3D_line(int x1, int y1, int z1, int x2, int y2, int z2);
+    setup_environment(int length, int width, int depth, int fiber_count, int fiber_length);
+};
