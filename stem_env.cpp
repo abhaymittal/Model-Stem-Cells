22084ec01cfb7aeaff88c54b378c4257572b3123
Abhay Mittal <abhaymittal@y7mail.com>
2014-06-08 01:49:46 +0530

    All initialization done in draw_3d_line

--- stem_env.cpp
+++ stem_env.cpp
@@ -33,5 +33,18 @@ stem_env::draw_3D_line(int x1, int y1, int z1, int x2, int y2, int z2)
             Var dep_val_2('z',2,abs(z2-z1),z1);
         }
     }
+
+
+    int counter_1=0, counter_2=0;
+
+    int **pt_array;
+    pt_array=new int*[ind_val.get_delta()+1];
+    for(int i=0;i<ind_val.get_delta()+1;i++)
+    {
+        pt_array[i]=new int[3];
+    }
+
+
+
 }
 
