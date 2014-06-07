30bf4275c9403a8c4a102727c533c682152c7661
Abhay Mittal <abhaymittal@y7mail.com>
2014-06-07 17:42:10 +0530

    Added return type and cpp for stem_env class methods

--- stem_env.h
+++ stem_env.h
@@ -2,6 +2,6 @@
 class stem_env
 {
 public:
-    draw_3D_line(int x1, int y1, int z1, int x2, int y2, int z2);
-    setup_environment(int length, int width, int depth, int fiber_count, int fiber_length);
+  int** draw_3D_line(int x1, int y1, int z1, int x2, int y2, int z2);
+  int setup_environment(int length, int width, int depth, int fiber_count, int fiber_length);
 };
