defa6377bbaab35d7ef529e5fdc01aef26b0eff4
Pratik Varshney <pratz.varz@gmail.com>
2014-06-14 11:41:51 +0530

    Modified main file to create .xyz format output file

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,24 @@ int main()
     Stem_env st;
     Line_3d *line;
     long ***pt_map;
-    ofstream output_file("pt_map.tsv");
-    line=st.setup_environment(20,20,20,1000,15);
-    pt_map=st.generate_pt_freq_map(line,20,20,20,1000);
+    ofstream output_file("pt_map.xyz");
+    int length = 20, width = 20, depth = 20, fiber_count = 1000, fiber_length = 15;
+
+    line=st.setup_environment(length,width,depth,fiber_count,fiber_length);
+    pt_map=st.generate_pt_freq_map(line,length,width,depth,fiber_count);
 
     if(output_file.is_open())
     {
-        for(int x=0;x<20;x++)
+        output_file<<(length*width*depth); //store num of points in .xyz file
+        output_file<<"\n"<<"Box Dimensions: length = "<<length<<", width = "<<width<<", depth = "<<depth; //comment line of .xyz file
+
+        for(int x=0;x<length;x++)
         {
-            for(int y=0;y<20;y++)
+            for(int y=0;y<width;y++)
             {
-                for(int z=0;z<20;z++)
+                for(int z=0;z<depth;z++)
                 {
-                    output_file<<pt_map[x][y][z]<<"\t"<<x<<"\t"<<y<<"\t"<<z<<endl;
+                    output_file<<"\n"<<pt_map[x][y][z]<<"\t"<<x<<"\t"<<y<<"\t"<<z;
                 }
             }
         }
