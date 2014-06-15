7a6729e1bad8526af5d05ba064b17f67c6c2b65a
Pratik Varshney <pratz.varz@gmail.com>
2014-06-15 18:06:46 +0530

    Added support for loading XML file: input.xml, Added notes.txt for requirement/compilation notes

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,57 @@
 #include "Line_3d.h"
 #include <iostream>
 #include<fstream>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <libxml++/libxml++.h>
+
 using namespace std;
-int main()
+int main(int argc, char* argv[])
 {
     Stem_env st;
     Line_3d *line;
     long ***pt_map;
+    int length, width, depth, fiber_length;
+    long fiber_count;
+
+    //Loading XML file
+
+    string input_file;
+    if(argc > 1)
+    {
+        input_file = argv[1]; //To specify input file as command line argument
+    }
+    else
+    {
+        input_file = "input.xml";
+    }
+
+    try
+    {
+        xmlpp::DomParser parser;
+        parser.parse_file(input_file);
+        if(parser)
+        {
+            const xmlpp::Node* pNode = parser.get_document()->get_root_node();
+
+            length = pNode->eval_to_number("//Parameters/Lattice/Length"); //returned double value is stored as int
+            width = pNode->eval_to_number("//Parameters/Lattice/Width");
+            depth = pNode->eval_to_number("//Parameters/Lattice/Depth");
+            fiber_length = pNode->eval_to_number("//Parameters/FiberLength");
+            fiber_count = pNode->eval_to_number("//Parameters/FiberCount");
+        }
+    }
+    catch(const exception& ex)
+    {
+        cout << "Exception: " << ex.what() << endl;
+    }
+
+    //End Loading XML file
+
     ofstream output_file("pt_map.xyz");
-    int length = 20, width = 20, depth = 20, fiber_count = 1000, fiber_length = 15;
 
     line=st.setup_environment(length,width,depth,fiber_count,fiber_length);
     pt_map=st.generate_pt_freq_map(line,length,width,depth,fiber_count);
