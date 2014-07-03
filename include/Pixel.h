fc58194229fd7200de51f09b724bb8d77eb6277f
Abhay Mittal <abhaymittal@y7mail.com>
2014-07-03 12:14:41 +0530

    Added Pixel class

---
+++ include/Pixel.h
@@ -0,0 +1,25 @@
+#ifndef PIXEL_H
+#define PIXEL_H
+
+
+class Pixel
+{
+    private:
+    int cellID;
+    public:
+        /** Default constructor */
+        Pixel();
+        /** Default destructor */
+        virtual ~Pixel();
+
+        long int getCellID() const {
+            return cellID;
+        }
+
+        int setCellID(long cellID_) {
+            cellID=cellID_;
+            return 0;
+        }
+};
+
+#endif // PIXEL_H
