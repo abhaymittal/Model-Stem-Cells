622c2548d57caeb9083bb904f02d877426927789
Abhay Mittal <abhaymittal@y7mail.com>
2014-07-16 01:58:11 +0530

    Automaton Cell => Moved definitions to cpp and documented .h

--- include/AutomatonCell.h
+++ include/AutomatonCell.h
@@ -1,47 +1,85 @@
-/*
- * AutomatonCell.h
- *
- *  Created on: Jun 18, 2014
- *      Author: sandeep
- */
+/**
+*   @class  AutomatonCell
+*   @brief  Class representing a Lattice Point
 
+*   The following are the legal values for the type
+*   Type    |    Meaning
+*   --------|-----------
+*   0       |    Nothing present
+*   1       |    ECM
+*   2       |    Cell
+
+*   The following are the legal values for cellID
+*   Cell ID |   Denotes
+*   --------|----------
+*   0       |   Cell not present
+*   >0      |   The id of the cell present
+
+*   @author Abhay Mittal
+*   @author Pratik Varshney
+*/
 #ifndef AUTOMATONCELL_H_
 #define AUTOMATONCELL_H_
 
 class AutomatonCell {
 private:
-    short unsigned int type;
-	short unsigned int cellCount;
-	long int ID;
+    short unsigned int type;        /**< Denotes the content at this point*/
+	short unsigned int cellCount;   /**< Number of fibers at this point if ECM, else 0*/
+	long int ID;                    /**< The cell id if a cell is present, else 0*/
 
 public:
+
+    /**
+    *   @brief  Default no argument constructor
+    */
 	AutomatonCell();
 
-	short unsigned int getType() const {
-		return type;
-	}
+    /**
+    *   @brief  Getter function for type
+    *   @return The type of the point
+    */
+	short unsigned int getType() const;
 
-	void setType(short unsigned int type) {
-		this->type = type;
-	}
+    /**
+    *   @brief  Setter function for the type
+    *   @param  type : The type of point
+    *   @return 0
+    */
+	int setType(short unsigned int type);
 
-	short unsigned int getCount() const {
-		return cellCount;
-	}
+    /**
+    *   @brief  Getter function for number of ECM fibers
+    *   @return Number of ECM fibers
+    */
+	short unsigned int getCount() const;
 
-	void setCount(short unsigned int cellCount) {
-		this->cellCount = cellCount;
-	}
+    /**
+    *   @brief  Setter function for number of ECM fibers
+    *   @param  cellCount:  The number of ECM fibers
+    *   @return 0
+    */
+	int setCount(short unsigned int cellCount);
 
-	long int getId() const {
-		return ID;
-	}
+	/**
+    *   @brief  Getter function for the cell ID
+    *   @return Cell ID
 
-	void setId(long int ID) {
-		this->ID = ID;
-	}
+    *        (if cell is present, else 0)
+    */
+	long int getId() const;
 
-	void incrementCount();
+    /**
+    *   @brief  Setter function for the cell ID
+    *   @param  ID: The id of the cell
+    *   @return 0
+    */
+	int setId(long int ID);
+
+    /**
+    *   @brief  Function to increment the number of fibers
+    *   @return 0
+    */
+	int incrementCount();
 
 };
 
--- src/AutomatonCell.cpp
+++ src/AutomatonCell.cpp
@@ -15,6 +15,33 @@ AutomatonCell::AutomatonCell() {
 }
 
 
-void AutomatonCell::incrementCount() {
+int AutomatonCell::incrementCount() {
     cellCount+=1;
+    return 0;
+}
+
+short unsigned int getType() const {
+		return type;
+}
+
+int setType(short unsigned int type) {
+		this->type = type;
+		return 0;
+}
+
+short unsigned int getCount() const {
+		return cellCount;
+}
+
+int setCount(short unsigned int cellCount) {
+		this->cellCount = cellCount;
+}
+
+
+long int getId() const {
+		return ID;
+}
+
+int setId(long int ID) {
+		this->ID = ID;
 }
