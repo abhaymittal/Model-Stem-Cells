256bab0c1bc38e84893ab4006047d6c9dcd9a61a
Abhay Mittal <abhaymittal@y7mail.com>
2014-10-31 12:27:00 +0530

    StemCell, TACell => Added documentation

--- include/StemCell.h
+++ include/StemCell.h
@@ -17,7 +17,30 @@ class StemCell : public Cell
 
         StemCell();
 
+        /**
+        *   @brief               Method for symmetric division of Stem Cell
+        *   @param[out]  daughterCell: The new daughter cell generated
+        *   @pram[in]        environment: The lattice
+        *   @pram[in]        sim: The simulation parameters
+        *   @return            The function returns the following values
+        *                               Return Value | Meaning
+        *                               -------------------|-------------
+        *                               0              | A daughter was created
+        *                               -1            | No suitable location was found
+        */
         int divide(StemCell& daughterCell, AutomatonCell ***environment, SimulationParameters sim);
+
+         /**
+        *   @brief               Method for asymmetric division of Stem Cell
+        *   @param[out]  daughterCell: The new daughter cell generated. It will be a Transit Amplifying Cell.
+        *   @pram[in]        environment: The lattice
+        *   @pram[in]        sim: The simulation parameters
+        *   @return            The function returns the following values
+        *                               Return Value | Meaning
+        *                               -------------------|-------------
+        *                               0              | A daughter was created
+        *                               -1            | No suitable location was found
+        */
         int divide(TACell& daughterCell, AutomatonCell*** environment,SimulationParameters sim);
 };
 
--- include/TACell.h
+++ include/TACell.h
@@ -1,3 +1,10 @@
+/**
+*   @class TACell
+*   @brief This class models the Transit Amplifying Cells and is derived from the Cell class
+*   @author Abhay Mittal
+*   @author Pratik Varshney
+*/
+
 #ifndef TACELL_H
 #define TACELL_H
 
@@ -8,6 +15,10 @@ class TACell : public Cell
 {
     public:
         TACell();
+        /**
+        *   @brief Function for differentiating  the Transit Amplifying Cell
+        *
+        */
     int differentiate();
 };
 
