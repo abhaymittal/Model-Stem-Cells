8b5bef480efa3d303556f343c350afded27e9314
Abhay Mittal <abhaymittal@y7mail.com>
2014-11-07 15:51:30 +0530

    StemCell.cpp => Corrected variable name in declaration

--- src/StemCell.cpp
+++ src/StemCell.cpp
@@ -6,7 +6,7 @@ StemCell::StemCell() : Cell()
 }
 
 
-int StemCell::divide(StemCell& daughterCell, AutomatonCell ***environment, SimulationParameters sim) {
+int StemCell::divide(StemCell& daughter, AutomatonCell ***environment, SimulationParameters sim) {
     int temp;
     Point childPoint;
     temp= getFavorableLocation(childPoint,environment, sim);
@@ -14,15 +14,15 @@ int StemCell::divide(StemCell& daughterCell, AutomatonCell ***environment, Simul
         return temp;
     daughter.setAge(0);
     daughter.setCentroid(childPoint);
-    daughter.setRadius(this.getRadius());
-    daughter.setSenseRadius(this.getSenseRadius());
+    daughter.setRadius(this->getRadius());
+    daughter.setSenseRadius(this->getSenseRadius());
     daughter.setEB(0);
     daughter.setECadherin(1.0F);
-    this.setAge(0);
+    this->setAge(0);
     return 0;
 }
 
-int StemCell::divide(TACell& daughterCell, AutomatonCell*** environment,SimulationParameters sim) {
+int StemCell::divide(TACell& daughter, AutomatonCell*** environment,SimulationParameters sim) {
     int temp;
     Point childPoint;
     temp= getFavorableLocation(childPoint,environment, sim);
@@ -30,10 +30,10 @@ int StemCell::divide(TACell& daughterCell, AutomatonCell*** environment,Simulati
         return temp;
     daughter.setAge(0);
     daughter.setCentroid(childPoint);
-    daughter.setRadius(this.getRadius());
-    daughter.setSenseRadius(this.getSenseRadius());
+    daughter.setRadius(this->getRadius());
+    daughter.setSenseRadius(this->getSenseRadius());
     daughter.setEB(0);
     daughter.setECadherin(1.0F);
-    this.setAge(0);
+    this->setAge(0);
     return 0;
 }
