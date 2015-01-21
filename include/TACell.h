78e75444ea4803a980b8f13b27698356d543e108
Abhay Mittal <abhaymittal@y7mail.com>
2015-01-21 17:36:12 +0530

    TACell => Added beta and its functions. Also wrote the divide function and half of differentiate

--- include/TACell.h
+++ include/TACell.h
@@ -13,13 +13,51 @@
 
 class TACell : public Cell
 {
-    public:
-        TACell();
-        /**
-        *   @brief Function for differentiating  the Transit Amplifying Cell
-        *
-        */
-    int differentiate();
+private:
+    int currentBeta;
+
+public:
+    TACell();
+
+    /**
+    *   @brief  Method to return the current value of Beta
+    *   @return The current value of Beta
+    */
+    int getCurrentBeta();
+
+    /**
+    *   @brief  Method to set the current value of Beta
+    *   @param  The new value of Beta
+    *   @return 0
+    */
+    int setCurrentBeta(int Beta);
+
+    /**
+    *   @brief  Method to increment the current value of Beta
+    *   @return 0
+    */
+    int incrementBeta();
+
+    /**
+    *   @brief Function for differentiating  the Transit Amplifying Cell
+    *   @param[out] TDC : The terminally differentiated cell to generate
+    *   @param[in,out] taCells : The deque containing all the TA cells in the environment
+    *   @return 0
+    */
+    int differentiate(Cell& TDC, std::deque<TACell> &tacells);
+
+    /**
+    *   @brief               Method for division of Transit Amplifying Cells
+    *   @param[out]  daughter: The new daughter cell generated
+    *   @pram[in]        environment: The simulation environment lattice
+    *   @pram[in]        sim: The simulation parameters
+    *   @return            The function returns the following values
+    *                               Return Value | Meaning
+    *                               -------------------|-------------
+    *                               0              | A daughter was created
+    *                               -1            | No suitable location was found
+    */
+    int divide(TACell& daughter, int id_, AutomatonCell ***environment, SimulationParameters sim);
 };
 
 #endif // TACELL_H
--- src/TACell.cpp
+++ src/TACell.cpp
@@ -1,6 +1,50 @@
 #include "TACell.h"
 
-TACell::TACell()
+TACell::TACell():Cell(),currentBeta(0)
 {
-    //ctor
+
+}
+
+int getCurrentBeta() {
+    return currentBeta;
+}
+int setCurrentBeta(int Beta) {
+    currentBeta=Beta;
+    return 0;
+}
+int incrementBeta() {
+    currentBeta=currentBeta+1;
+    return 0;
+}
+
+int differentiate(Cell& TDC, std::deque<TACell> &tacells) {
+    TDC.setAge(0);
+    TDC.setCentroid(this->getCentroid());
+    TDC.setRadius(this->getRadius());
+    TDC.setSenseRadius(this->getSenseRadius());
+}
+
+
+int TACell::divide(TACell& daughter, int id_, AutomatonCell ***environment, SimulationParameters sim) {
+
+    Point childPoint;
+    int status =getFavorableLocation(childPoint, environment, sim);
+
+    if(status!=0) return status;
+
+    daughter.setCentroid(childPoint);
+    daughter.setId(id_);
+    daughter.setRadius(this->getRadius());
+    daughter.setSenseRadius(this->getSenseRadius());
+    daughter.setECadherin(1.0F);
+    daughter.setEB(0.0F);
+    daughter.setAge(0);
+    this->setAge(0);
+    this->incrementBeta();
+    daughter.setCurrentBeta(this->getCurrentBeta());
+    environment[childPoint.getX()][childPoint.getY()][childPoint.getZ()].setType(AutomatonCell::CELL);
+    environment[childPoint.getX()][childPoint.getY()][childPoint.getZ()].setCount(0);
+    environment[childPoint.getX()][childPoint.getY()][childPoint.getZ()].setId(daughter.getId());
+    return 0;
+
 }
