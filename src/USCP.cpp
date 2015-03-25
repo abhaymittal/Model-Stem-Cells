7021292bcba1cc3df8015e121661cfccb56e5a0d
Abhay Mittal <abhaymittal@y7mail.com>
2015-03-25 14:25:30 +0530

    USCP , PARAVIEW => Made modifications in simulation and changed radius

--- src/USCP.cpp
+++ src/USCP.cpp
@@ -56,7 +56,7 @@ int main() {
     }
 	env.setupECM(sim,environment);
 
-	int sphereRadius = 20;
+	int sphereRadius = 10;
 	int cellRadius = 1;
 	env.clearSphericalRegion(sim.getLatticeWidth()/2, sim.getLatticeHeight()/2, sim.getLatticeDepth()/2, sphereRadius, environment);
 
@@ -140,7 +140,7 @@ int main() {
         /* collecting data for plotting vs time(iteration) */
 
         /* Reset values */
-        numStemCells = 0;
+     /*   numStemCells = 0;
         numTACells = 0;
         numNormalCells = 0;
         numTotalCells = 0;
@@ -175,7 +175,7 @@ int main() {
         numNormalCellsFile << itr << "," << numNormalCells << endl;
         numTotalCellsFile << itr << "," << numTotalCells << endl;
         averageEBFile << itr << "," << averageEB << endl;
-        totalECMFiberFile << itr << "," << totalECMFiber << endl;
+        totalECMFiberFile << itr << "," << totalECMFiber << endl;*/
 
         /* End data collection for ith iteration */
         util.generateECMFile(sim, environment, Utilities::TYPE, itr);
--- uscp_sim.py
+++ uscp_sim.py
@@ -6,13 +6,13 @@ paraview.simple._DisableFirstRenderCameraReset()
 #Creating a render view to display animation
 renderView=CreateView('RenderView')
 renderView.ViewSize = [1217, 837]
-renderView.CenterOfRotation = [49.5, 49.5, 49.5]
+renderView.CenterOfRotation =  [19.5, 19.5, 19.5]
 renderView.StereoType = 0
-renderView.CameraPosition = [0.2533115342812731, -7.2933230751984865, 372.1186169832492]
-renderView.CameraFocalPoint = [49.50000000000002, 49.50000000000002, 49.50000000000002]
-renderView.CameraViewUp = [0.49586925413235977, 0.8391650398537878, 0.22341825953446165]
+renderView.CameraPosition =  [65.26351752062817, 119.05851804582377, -64.75015285371589]
+renderView.CameraFocalPoint = [38.14575246574428, 63.652794990600675, -3.072600922872282]
+renderView.CameraViewUp = [0.9491352449466381, -0.2466000227945012, 0.19578231676491256]
 renderView.CameraParallelScale = 85.73651497465943
-renderView.Background = [0.32, 0.34, 0.43]
+renderView.Background = [0.0, 0.0, 0.0]
 #Creating a CSV Reader
 fileList=[];
 a=int(raw_input("Enter the number of iteration files"));
@@ -29,13 +29,13 @@ tableToPointsFilter.ZColumn='z'
 
 scalarLUT = GetColorTransferFunction('type')
 scalarLUT.EnableOpacityMapping = 1
-scalarLUT.RGBPoints = [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 2.0, 0.0, 1.0, 0.0, 3.0, 1.0, 0.0, 0.0, 4.0, 1.0, 1.0, 1.0]
+scalarLUT.RGBPoints =[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 2.0, 0.0, 1.0, 0.0, 3.0, 1.0, 0.0, 0.0, 4.0, 1.0, 1.0, 1.0]
 scalarLUT.NanColor = [0.500008, 0.0, 0.0]
 scalarLUT.ScalarRangeInitialized = 1.0
 
 
 scalarPWF = GetOpacityTransferFunction('type')
-scalarPWF.Points = [0.0, 0.0, 0.5, 0.0, 1.0, 1.0, 0.5, 0.0, 2.0, 1.0, 0.5, 0.0, 3.0, 1.0, 0.5, 0.0, 4.0, 1.0, 0.5, 0.0]
+scalarPWF.Points =[0.0, 0.0, 0.5, 0.0, 1.0, 0.3, 0.5, 0.0, 2.0, 0.7, 0.5, 0.0, 3.0, 0.7, 0.5, 0.0, 4.0, 1.0, 0.5, 0.0]
 scalarPWF.ScalarRangeInitialized = 1
 
 #Setup vizualization
@@ -43,7 +43,7 @@ scalarPWF.ScalarRangeInitialized = 1
 tableToPointsDisplay=Show(tableToPointsFilter,renderView)
 tableToPointsDisplay.ColorArrayName=['POINTS', 'type']
 tableToPointsDisplay.LookupTable=scalarLUT
-tableToPointsDisplay.PointSize = 1.0
+tableToPointsDisplay.PointSize = 2.0
 
 #show color legend
 tableToPointsDisplay.SetScalarBarVisibility(renderView, True)
@@ -59,4 +59,4 @@ envOutline = Outline(Input=tableToPointsFilter)
 #display the outline
 envOutDisplay = Show(envOutline, renderView)
 envOutDisplay.ColorArrayName = [None, '']
-envOutDisplay.PointSize = 1.0
+envOutDisplay.PointSize = 2.0
